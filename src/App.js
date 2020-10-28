8586668fbab001e144a3e741b24a37e78600ecbf
Ross Keenan <rkeenan@auroracoop.com>
2020-10-28 09:32:18 -0500

    shortened post object. changed passing of props to the property names

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Post from './components/Post'
 
+const post = {
+  likes: 20,
+  description: "This is a post",
+  image: {
+    url: "/uploads/ali_karimi_0_N1_C_Nx_Oqw_I0_unsplash_f0855a2dfe.jpg"
+  } 
+}
+
+
+
 function App() {
   return (
     < div className="App">
+      <Post 
+        likes={post.likes}
+        description={post.description}
+        url={post.image && post.image.url}
+        />
       <Post/>
     </div>
   );
