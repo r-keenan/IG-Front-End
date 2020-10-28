85cd5491100b7f0e0efc96943ca74f326a692b4d
Ross Keenan <rkeenan@auroracoop.com>
2020-10-28 14:54:38 -0500

    Form has been created that submits to Strapi and creates a post.

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default () => (
-    <div className='Create'>
-        Create
+export default () => {
+
+    const [description, setDescription] = useState('')
+
+    const handleSubmit = async (event) => {
+        /*This prevents it from reloading the same page when submiting*/
+        event.preventDefault();
+
+        const response = await fetch('http://localhost:1337/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                description
+            })
+        })
+        const data = await response.json()
+
+        console.log("data", data)
+    }
+
+    return(
+        <div className='Create'>
+        <h2>Create</h2>
+
+        <form onSubmit={handleSubmit}>
+            <input 
+                placeholder="Description"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+            />
+            <button>Submit</button>
+        </form>
     </div>
-)
+    )
+}
