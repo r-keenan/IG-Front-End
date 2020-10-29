73bb7f80c77fe498f109fa592220d6f1f9eba19d
Ross Keenan <rkeenan@auroracoop.com>
2020-10-29 14:53:54 -0500

    Added front end validation and reset error messages on front end when user starts typing into the description field.

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -4,6 +4,7 @@ export default () => {
 
     const [description, setDescription] = useState('')
     const [file, setFile] = useState(null)
+    const [error, setError] = useState('')
 
     console.log("file", file)
 
@@ -11,33 +12,55 @@ export default () => {
         /*This prevents it from reloading the same page when submiting*/
         event.preventDefault()
 
+        if(!description){
+            setError('Please a description')
+            return
+        }
+        if(!file){
+            setError('Please add a File')
+            return
+        }
+
         const formData = new FormData()
         formData.append('data', JSON.stringify({description}))
         formData.append('files.image', file)
 
-        const response = await fetch('http://localhost:1337/posts', {
-            method: 'POST',
-            body: formData
-        })
-        const data = await response.json()
+        try{
+            const response = await fetch('http://localhost:1337/posts', {
+                method: 'POST',
+                body: formData
+            })
+            const data = await response.json()
 
-        console.log("data", data)
+            console.log("data", data)
+        } catch(err){
+            console.log("Exception ", err)
+            setError(err)
+        }
     }
 
     return(
         <div className='Create'>
         <h2>Create</h2>
 
+        {error && <p>{error}</p>}
+
         <form onSubmit={handleSubmit}>
             <input 
                 placeholder="Description"
                 value={description}
-                onChange={(event) => setDescription(event.target.value)}
+                onChange={(event) => {
+                    setError('')    
+                    setDescription(event.target.value)
+                }}
             />
             <input
                 type='file'
                 placeholder='Add a File'
-                onChange={(event) => setFile(event.target.files[0])}
+                onChange={(event) => {
+                    setError('')
+                    setFile(event.target.files[0])
+                }}
             />
             <button>Submit</button>
         </form>
