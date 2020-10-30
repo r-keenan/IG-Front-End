d493b4df93547ab24fd26ce4b0a1742017b769f0
Ross Keenan <rkeenan@auroracoop.com>
2020-10-30 11:14:44 -0500

    Added fully functioning Edit and Delete buttons and capabilities.

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,8 +1,93 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Post from '../components/Post';
 
-export default () => (
-    <div>
-        <br/>
-        SinglePost
-    </div>
-)
+export default ({match, history}) => {
+
+    const {id} = match.params
+    console.log("id", id)
+
+    const [post, setPost] = useState({})
+    const [loading, setLoading] = useState(true)
+    const [edit, setEdit] = useState(false)
+    //Used for the edit form
+    const [description, setDescription] = useState('')
+
+    const fetchPost = async () => {
+        const response = await fetch(`http://localhost:1337/posts/${id}`)
+        const data = await response.json()
+
+        console.log("data", data)
+        setPost(data)
+        setDescription(data.description)
+        setLoading(false)
+    }
+
+    const handleDelete = async () => {
+        const response = await fetch(`http://localhost:1337/posts/${id}`, 
+        {method: 'DELETE'
+        })
+        const data = await response.json()
+        history.push('/')
+    }
+
+    const handleEditSubmit = async (event) => {
+        event.preventDefault()
+        console.log("handleEditSubmit")
+
+        const response = await fetch(`http://localhost:1337/posts/${id}`, {
+            method: "PUT",
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                description
+            })
+            
+        })
+        const data = await response.json()
+        fetchPost()
+        console.log("handleEditSubmit data", data)
+    }
+
+    useEffect(() => {
+        fetchPost()
+    /*The square brackets below stop this from becoming infinite GET requests.*/
+    }, [])
+    
+    return(
+        <div>
+            <br/>
+            {loading &&
+                <p>Loading.....</p>
+            }
+            {!loading &&
+                <>
+                    {post.id && 
+                        <>
+                            <Post
+                                description={post.description}
+                                url={post.image && post.image.url}
+                                likes={post.likes}
+                            />
+                            <button onClick={handleDelete}>Delete this Post</button>
+                            <button onClick={() => setEdit(true)}>Edit this Post</button>
+                            {edit &&
+                                <form onSubmit={handleEditSubmit}>
+                                    <input 
+                                        value={description}
+                                        onChange={(event) => setDescription(event.target.value)} 
+                                        placeholder="New description"
+                                    />
+                                    <button>Confirm</button>
+                                </form>
+                            }
+                        </>
+                    }
+                    {!post.id &&
+                        <p>404 Error - post not found</p>
+                    }
+                </>
+            }
+        </div>
+    )
+}
